68ee0f1b5ab5ac91054a70888af6e6d6b5f9eecb
AhmadAttalhaoui <61194391+AhmadAttalhaoui@users.noreply.github.com>
2021-10-11 21:17:31 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace List
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btAdd = new System.Windows.Forms.Button();
+            this.tbList = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(109, 160);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(75, 23);
+            this.btAdd.TabIndex = 0;
+            this.btAdd.Text = "Add";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // tbList
+            // 
+            this.tbList.Location = new System.Drawing.Point(109, 131);
+            this.tbList.Name = "tbList";
+            this.tbList.Size = new System.Drawing.Size(306, 23);
+            this.tbList.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbList);
+            this.Controls.Add(this.btAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.TextBox tbList;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List
+{
+    public partial class Form1 : Form
+    {
+        List<string> lijst = new List<string>();
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            
+
+            lijst.Add(tbList.Text);
+
+            string s = String.Join(",", lijst);
+
+            MessageBox.Show(s.ToString());
+        }
+    }
+}
